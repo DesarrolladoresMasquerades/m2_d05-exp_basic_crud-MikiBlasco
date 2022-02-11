14bd20737545a28e69526357b804b4029d6e6edb
Miki Blasco <blascom@7@gmail.com>
2022-02-11 16:58:02 +0100

    lesson done

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/library-project')
+  .connect('mongodb+srv://Miki:ironhack@cluster0.lpnm2.mongodb.net/library-project?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
   .then(x =>
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
